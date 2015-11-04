89adb66139962d077fffb4cf2f70d319a1c90a85
Richard Hazelden <dhazeldend@gmail.com>
2015-11-04 14:38:05 +0200

    banner tweak and remove hover

--- src/app/static/UI/stages/girls/girls.js
+++ src/app/static/UI/stages/girls/girls.js
@@ -26,14 +26,6 @@
 
         initialize : function(){
 
-            $(".girls-snowflake").hover(
-                function(){
-                    $(this).find(".char-wrap").addClass("hover", 1000);
-                }, function(){
-                    $(this).find(".char-wrap").removeClass("hover", 1000);
-                }
-            );
-
         }
 
     };
